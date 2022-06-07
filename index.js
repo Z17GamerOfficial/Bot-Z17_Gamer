f8d3294c01609d7eada48eab4f0b78c33daec36e
Dragone1234 <101521860+Dragone1234@users.noreply.github.com>
2022-06-07 16:10:05 +0200

    Upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_SCHEDULED_EVENTS"]}
 )
 
-client.login("OTgxMjA4Mzc1NDk5MDM4Nzgw.Gptpzw.roMacUZaaxWT6w3Fjta9_aUVqzZY_mwtJiDvq0");
+client.login(process.eventNames.token);
 
 client.on("ready", () => {
     console.log("Z17_Gamer");
